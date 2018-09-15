fd48dcd4e04d7e62f089c0e651deef531315239d
saul9402 <saul_harry_@hotmail.com>
2018-09-14 23:43:54 -0500

    Se agregan dos java beans y la conexion a base de datos junto con el conector mysql

---
+++ src/main/java/mx/com/lickodev/ecommerce/javabeans/Marca.java
@@ -0,0 +1,53 @@
+package mx.com.lickodev.ecommerce.javabeans;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Santa Cecilia
+ */
+public class Marca implements Serializable {
+
+    private int codigo;
+    private String nombre;
+    private boolean visible;
+
+    public Marca() {
+    }
+
+    public Marca(int codigo, String nombre, boolean visible) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.visible = visible;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    @Override
+    public String toString() {
+        return "Marca{" + "codigo=" + codigo + ", nombre=" + nombre + ", visible=" + visible + '}';
+    }
+
+}
---
+++ src/main/java/mx/com/lickodev/ecommercejee/cad/Conexion.java
@@ -0,0 +1,25 @@
+package mx.com.lickodev.ecommercejee.cad;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Santa Cecilia
+ */
+public class Conexion {
+
+    public static Connection conectar() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            return DriverManager.getConnection("jdbc:mysql://localhost:3306/ecommerce_db?zeroDateTimeBehavior=convertToNull", "root", "");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(Conexion.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+
+}
